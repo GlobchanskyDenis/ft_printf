26ccf74a53d5275036167b960de4f7aa9675614a
Kirill <kirill@MacBook-Pro-Kirill.local>
2019-08-18 14:18:53 +0300

    oct done

--- type_oct.c
+++ type_oct.c
@@ -6,35 +6,81 @@
 /*   By: kirill <kirill@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/10 19:07:38 by forange-          #+#    #+#             */
-/*   Updated: 2019/08/13 16:21:25 by bsabre-c         ###   ########.fr       */
+/*   Updated: 2019/08/18 14:13:00 by kirill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int				ft_gen_oct(unsigned long long in, t_printf *tprint)
+static char			*gen_help(t_printf *tprint, int len, unsigned long long in)
 {
-	char				*filler;
-	char				*digit;
-	int					len;
+	char			*out;
+	char			*temp;
+	char			*prefix;
 
-	digit = ft_strjoin(tprint->flag & F_HASH ? "0" : "", \
-		(filler = ft_ulltoa_base(in, 8)));
-	free(filler);
-	len = ft_strlen(digit);
-	len = (tprint->flag & F_PREC && len < tprint->prec) ? tprint->prec : len;
-	filler = ft_strnew(len > tprint->width ? len : tprint->width);
-	ft_strdel(&digit);
-	ft_strdel(&filler);
-	return (len);
+	prefix = NULL;
+	temp = NULL;
+	out = NULL;
+	tprint->flag & F_PREC && len > tprint->prec ? tprint->flag &= ~F_ZERO : 0;
+	if (tprint->flag & F_HASH && in)
+	{
+		prefix = ft_strdup("0");
+		len += 1;
+	}
+	if (tprint->flag & F_PREC && tprint->prec > len)
+		temp = ft_strnewc(tprint->prec - len, '0');
+	else if (tprint->flag & F_ZERO && tprint->width > len)
+		temp = ft_strnewc(tprint->width - len, '0');
+	if (prefix && temp)
+		out = ft_strjoin(prefix, temp);
+	if (out)
+	{
+		free(prefix);
+		free(temp);
+		return (out);
+	}
+	return (prefix ? prefix : temp);
 }
 
-int						ft_oct_type(t_printf *tprint)
+static int			ft_gen_oct(unsigned long long in, t_printf *tprint)
+{
+	char			*filler;
+	char			*prefix;
+	char			*out;
+	int				len;
+
+	if (!in && tprint->flag & F_PREC && !tprint->prec)
+		filler = ft_strdup("");
+	else
+		filler = ft_ulltoa_base(in, 8);
+	prefix = gen_help(tprint, ft_strlen(filler), in);
+	out = ft_strjoin(prefix ? prefix : "", filler);
+	ft_strdel(&prefix);
+	ft_strdel(&filler);
+	if (tprint->flag & F_UP)
+		ft_strupr(out);
+	if ((len = ft_strlen(out)) >= tprint->width)
+		write(tprint->fd, out, len);
+	else
+	{
+		filler = ft_strnewc(tprint->width, ' ');
+		if (tprint->flag & F_MINUS)
+			ft_memcpy(filler, out, len);
+		else
+			ft_strcpy(filler + tprint->width - len, out);
+		write(tprint->fd, filler, tprint->width);
+		ft_strdel(&filler);
+	}
+	return (len > tprint->width ? len : tprint->width);
+}
+
+int					ft_oct_type(t_printf *tprint)
 {
 	unsigned long long	out;
 
 	tprint->str++;
-	tprint->flag & (F_PREC | F_ZERO) ? tprint->flag &= ~F_ZERO : 0;
+	tprint->flag & F_PREC && tprint->flag & F_ZERO ? \
+		tprint->flag &= ~F_ZERO : 0;
 	if (tprint->flag & F_PREC && !tprint->prec && !tprint->width)
 		return (0);
 	if (tprint->flag & L_HH)
