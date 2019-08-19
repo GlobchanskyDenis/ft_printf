71d9bfbd51ca7760d602646aba36be9b8711888e
Kirill <kirill@MacBook-Pro-Kirill.local>
2019-08-19 09:42:10 +0300

    staging

--- libft/ft_lltoa_base.c
+++ libft/ft_lltoa_base.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lltoa_base.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: forange- <forange-@student.fr.42>          +#+  +:+       +#+        */
+/*   By: kirill <kirill@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/10 17:17:29 by forange-          #+#    #+#             */
-/*   Updated: 2019/08/18 00:32:04 by forange-         ###   ########.fr       */
+/*   Updated: 2019/08/18 21:33:05 by kirill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,24 @@ char			*ft_lltoa_base(long long dgt, int base)
 	long long	temp;
 	int			i;
 	char		*out;
+	int			is_neg;
 
-	i = 0;
+	i = 1;
+	is_neg = dgt < 0 ? 1 : 0;
 	if (base < 2 && base > 16)
 		return (NULL);
 	if (!dgt)
 		return (ft_strdup("0"));
 	temp = ft_absll(dgt);
-	while (temp)
-	{
-		temp /= base;
+	while ((temp /= base))
 		i++;
-	}
-	out = (char*)ft_memalloc(i + (dgt < 0 ? 2 : 1));
+	out = (char*)ft_memalloc(i + is_neg + 1);
 	out[0] = dgt < 0 ? '-' : 0;
 	dgt = ft_absll(dgt);
 	while (dgt)
 	{
 		temp = dgt % base;
-		out[--i] = (temp > 9) ? (temp - 10) + 'a' : temp + '0';
+		out[--i + is_neg] = (temp > 9) ? (temp - 10) + 'a' : temp + '0';
 		dgt /= base;
 	}
 	return (out);
--- type_int.c
+++ type_int.c
@@ -6,14 +6,88 @@
 /*   By: kirill <kirill@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/18 17:51:13 by kirill            #+#    #+#             */
-/*   Updated: 2019/08/18 17:51:30 by kirill           ###   ########.fr       */
+/*   Updated: 2019/08/18 21:19:03 by kirill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static char			*gen_help(t_printf *tprint, int len, long long in)
+{
+	char			*temp;
+	char			*prefix;
+	char			*out;
+
+	temp = NULL;
+	prefix = NULL;
+	out = NULL;
+	tprint->flag & F_PREC && len > tprint->prec ? tprint->flag &= ~F_ZERO : 0;
+	if (tprint->flag & (F_PLUS | F_SPACE) && in >= 0)
+	{
+		prefix = ft_strdup(tprint->flag & F_PLUS ? "+" : " ");
+		len += 1;
+	}
+	if (tprint->flag & F_PREC && tprint->prec > (prefix ? len - 1 : len))
+		temp = ft_strnewc(tprint->prec - (prefix ? len - 1 : len), '0');
+	else if (tprint->flag & F_ZERO && tprint->width > len)
+		temp = ft_strnewc(tprint->width - len, '0');
+	if (prefix && temp)
+		out = ft_strjoin(prefix, temp);
+	if (out)
+	{
+		free(prefix);
+		free(temp);
+		return (out);
+	}
+	return (prefix ? prefix : temp);
+}
+
+static int	ft_gen_int(long long in, t_printf *tprint)
+{
+	char	*filler;
+	char	*out;
+	char	*prefix;
+	int 	len;
+
+	if (!in && tprint->flag & F_PREC && !tprint->prec)
+		filler = ft_strdup("");
+	else
+		filler = ft_lltoa_base(in, 10);
+	prefix = gen_help(tprint, ft_strlen(filler), in);
+	out = ft_strjoin(prefix ? prefix : "", filler);
+	ft_strdel(&prefix);
+	ft_strdel(&filler);
+	if ((len = ft_strlen(out)) >= tprint->width)
+		write(tprint->fd, out, len);
+	else
+	{
+		filler = ft_strnewc(tprint->width, ' ');
+		if (tprint->flag & F_MINUS)
+			ft_memcpy(filler, out, len);
+		else
+			ft_strcpy(filler + tprint->width - len, out);
+		write(tprint->fd, filler, tprint->width);
+		ft_strdel(&filler);
+	}
+	return (len > tprint->width ? len : tprint->width);
+}
+
 int			ft_int_type(t_printf *tprint)
 {
-	(void)tprint;
-	return (0);
+	long long	out;
+
+	tprint->str++;
+	tprint->flag & F_PREC && tprint->flag & F_ZERO ? \
+		tprint->flag &= ~F_ZERO : 0;
+	if (tprint->flag & L_HH)
+		out = (char)va_arg(tprint->args, int);
+	else if (tprint->flag & L_H)
+		out = (short)va_arg(tprint->args, int);
+	else if (tprint->flag & L_L)
+		out = va_arg(tprint->args, long);
+	else if (tprint->flag & L_LL)
+		out = va_arg(tprint->args, long long);
+	else
+		out = va_arg(tprint->args, int);
+	return (ft_gen_int(out, tprint));
 }
